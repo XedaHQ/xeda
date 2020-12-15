4670a17dce7857f484836e939c7b452d66f83ac9
Kamyar Mohajerani <kammoh@gmail.com>
2020-12-15 15:44:51 -0500

    fix restructure of vivado flow

--- xeda/cli.py
+++ xeda/cli.py
@@ -1,8 +1,3 @@
-from .flows.diamond import Diamond
-from .flows.vivado import Vivado
-from .flows.quartus import Quartus
-from .flows.ghdl import Ghdl
-
 from .xeda_app import XedaApp
 
 def run_xeda(args=None):
--- xeda/flows/vivado/__init__.py
+++ xeda/flows/vivado/__init__.py
@@ -1,4 +1,4 @@
 # © 2020 [Kamyar Mohajerani](mailto:kamyar@ieee.org)
 
-from .vivado import VivadoSim, VivadoPostsynthSim, VivadoPower, VivadoPowerLwc
+from .vivado_sim import VivadoSim, VivadoPostsynthSim, VivadoPower, VivadoPowerLwc
 from .vivado_synth import VivadoSynth
