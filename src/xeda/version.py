ed564742ac62de54103ae6957b37d24e60cd8a63
Kamyar Mohajerani <kammoh@gmail.com>
2022-08-26 14:41:50 -0400

    update dependencies and try fix version issue

--- src/xeda/version.py
+++ src/xeda/version.py
@@ -1,3 +1,11 @@
-from importlib.metadata import version
-
-__version__ = version(__package__)
+from importlib.metadata import version, PackageNotFoundError
+from pkg_resources import get_distribution, DistributionNotFound
+try:
+    __version__ = version("xeda")
+except PackageNotFoundError:
+    # package is not installed
+    try:
+        __version__ = get_distribution("xeda").version
+    except DistributionNotFound:
+         # package is not installed
+        pass
