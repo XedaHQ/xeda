c84f2d1518957d6aa6c140c2eab35d9571cb04bb
Kamyar Mohajerani <kammoh@gmail.com>
2022-08-26 14:53:22 -0400

    fixing CI...

--- src/xeda/version.py
+++ src/xeda/version.py
@@ -1,5 +1,6 @@
 from importlib.metadata import version, PackageNotFoundError
 from pkg_resources import get_distribution, DistributionNotFound
+
 try:
     __version__ = version("xeda")
 except PackageNotFoundError:
@@ -7,5 +8,5 @@ except PackageNotFoundError:
     try:
         __version__ = get_distribution("xeda").version
     except DistributionNotFound:
-         # package is not installed
+        # package is not installed
         pass
