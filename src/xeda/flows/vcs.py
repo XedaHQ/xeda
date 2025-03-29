6e4be5f1b777d1f6ec5b16873bf9389e47cc3a33
Kamyar Mohajerani <kammoh@gmail.com>
2025-03-29 14:12:58 -0400

    [wip] VCS

--- src/xeda/flows/vcs.py
+++ src/xeda/flows/vcs.py
@@ -4,6 +4,7 @@ from typing import List, Optional
 
 from ..tool import Tool
 from ..flow import SimFlow
+from ..design import SourceType
 
 log = logging.getLogger(__name__)
 
@@ -35,6 +36,7 @@ class Vcs(SimFlow):
                     f"default : {ss.work_dir}",
                 ]
             )
+
         vlogan_args = deepcopy(ss.vlogan_flags)
         vhdlan_args = deepcopy(ss.vhdlan_flags)
         if ss.work_dir:
@@ -47,13 +49,25 @@ class Vcs(SimFlow):
         incdirs: List[str] = []
         for d in incdirs:
             vlogan_args.append(f"+incdir+{d}")
-        vhdlan_args.append("-cycle")  # ???
-        vhdlan_args.extend("-event")  # ???
+        # vhdlan_args.append("-cycle")  # ???
+        # vhdlan_args.extend("-event")  # ???
         if self.design.language.vhdl.standard in ("08", "2008"):
             vhdlan_args.extend("-vhdl08")
 
         # FIXME
-        vlogan_args += ["-sverilog", "+v2k"]
+        vlogan_args += [, ]
+        vlog_files = self.design.sources_of_type(SourceType.Verilog)
+        if vlog_files:
+            log.info("analyzing Verilog files")
+            self.vlogan.run(*vlogan_args, "+v2k", *(str(f) for f in vlog_files))
+        sv_files = self.design.sources_of_type(SourceType.SystemVerilog)
+        if sv_files:
+            log.info("analyzing SystemVerilog files")
+            self.vlogan.run(*vlogan_args, "-sverilog", *(str(f) for f in sv_files))
+        vhdl_files = self.design.sources_of_type(SourceType.Vhdl)
+        if vhdl_files:
+            log.info("analyzing VHDL files")
+            self.vhdlan.run(*vhdlan_args, *(str(f) for f in vhdl_files))
         top = self.design.tb.top[0]
         vcs_args = [top, *ss.vcs_flags]
         vcs_args += [
@@ -64,16 +78,11 @@ class Vcs(SimFlow):
             vcs_args.append(f"-j{ss.nthreads}")
         if ss.vcs_log_file:
             vcs_args += ["-l", ss.vcs_log_file]
-
-        log.info("analyzing Verilog files")
-        self.vlogan.run(*vlogan_args)
-        log.info("analyzing VHDL files")
-        self.vhdlan.run(*vhdlan_args)
         log.info("Running vcs")
         self.vcs.run(*vcs_args)
 
         simv = Tool("./simv")  # ???
-        simv_args = ss.simv_flags
+        simv_args = deepcopy(ss.simv_flags)
         for k, v in self.design.tb.parameters.items():
             simv_args.append(f"+define+{k}={v}")
         simv_args.extend(["-l", top])
