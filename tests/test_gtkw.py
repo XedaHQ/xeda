342b75b24a1cd34a4c64af0e50bb773d4322e780
Kamyar Mohajerani <kammoh@gmail.com>
2025-04-01 15:43:56 -0400

    [test] removed gtkwave

--- tests/test_gtkw.py
+++ tests/test_gtkw.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from xeda.flows.ghdl import gen_gtkw, _get_wave_opt_signals, common_root
+from xeda.flows.ghdl import _get_wave_opt_signals, common_root
 
 
 TESTS_DIR = Path(__file__).parent.absolute()
@@ -8,20 +8,11 @@ RESOURCES_DIR = TESTS_DIR / "resources"
 
 def test_gen_gtkw():
     opt_file = RESOURCES_DIR / "wave.opt"
-    dump_file = "debug.ghw"
     extra_top = "top"
 
     signals, root_group = _get_wave_opt_signals(opt_file, extra_top)
     assert len(signals) > 1
     assert len(root_group) == 2
-    gen_gtkw(dump_file, signals, root_group)
-    gtkw = Path("debug.gtkw")
-    assert gtkw.exists()
-    with open(gtkw) as f:
-        lines = f.read().splitlines()
-    for s in signals:
-        sig_name = ".".join(s)
-        assert lines.count(sig_name) > 0, f"{sig_name} not in lines"
 
 
 def test_common_root():
