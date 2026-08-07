47f93315691156b9943de675dca618dd72897bd2
Kamyar Mohajerani <kammoh@gmail.com>
2026-08-07 17:11:33 -0400

    Default to --no-cached-dependencies

--- src/xeda/cli.py
+++ src/xeda/cli.py
@@ -177,7 +177,7 @@ def list_settings(ctx: click.Context, flow):
 )
 @click.option(
     "--cached-dependencies/--no-cached-dependencies",
-    default=True,
+    default=False,
     help="Don't run dependency flows if a previous successfull run on the same design and flow settings exists. Generated directory names will contain a hash of design and/or flow settings.",
 )
 @click.option(
